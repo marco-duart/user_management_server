a4d62d1c8f310651b30591070e91e8a5cc731889
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-05-09 17:44:56 -0300

    feat: added database config

---
+++ src/database/database.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import typeOrmConfigAsync from './database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRootAsync(typeOrmConfigAsync)],
+})
+export class DatabaseModule {}
