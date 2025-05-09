5b0a9a99fc0323c6652667d649a4c44bcc3346c7
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-05-09 17:44:18 -0300

    feat: config main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('User Management Server')
+    .setDescription('This is an API')
+    .setVersion('1.0')
+    .addServer('/v1')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('v1/docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('v1');
+  app.enableCors({
+    origin: '*',
+  });
+
+  await app.listen(configService.get('PORT') || 3000);
 }
+
 bootstrap();
