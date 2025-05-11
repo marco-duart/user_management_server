9b815717111933d109f77d9b79bdc247e99ce3b3
marco-duart <marcoaurelio_db@hotmail.com>
2025-05-11 20:47:01 -0300

    feat: updated exception message

--- src/enums/exception-message.enum.ts
+++ src/enums/exception-message.enum.ts
@@ -2,7 +2,6 @@ export enum EXCEPTION_MESSAGE {
   EMAIL_EXISTS = 'Email already registered.',
   USER_NOT_FOUND = 'User not found.',
   WRONG_CREDENTIALS = 'Email or password wrong.',
-  USER_UPDATE_NOT_ALLOWED = 'You are not allowed to update this user.',
-  DELETE_USER_NOT_ALLOWED = 'You are not allowed to delete this user.',
+  FORBIDDEN = 'Forbidden resource',
   INCORRECT_ARGUMENTS = 'Incorrect arguments or params.',
 }
