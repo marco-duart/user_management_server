99f2b02e3930c939757b47fc7bf8938eb1d49444
Marco Aurélio <marcoaurelioduartebezerra@gmail.com>
2025-05-16 17:36:46 -0300

    feat: added passport module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtOptions } from './jwt/jwt.config';
+import { PassportModule } from '@nestjs/passport';
 import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({ ...jwtOptions, global: true }),
+    PassportModule.register({ defaultStrategy: 'google' }),
   ],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy],
